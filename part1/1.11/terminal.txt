63fbdcde77a3a28a677d233c1c5e141f79219b81
Veeti <veeti.koivuniemi@gmail.com>
2023-06-04 23:38:09 +0300

    part1 done

---
+++ part1/1.11/terminal.txt
@@ -0,0 +1,1 @@
+docker build . -t spring-project && docker run -p 8080:8080 spring-project
---
+++ part1/1.7/websitescript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+while true
+do
+  echo "Input website:"
+  read website; echo "Searching.."
+  sleep 1; curl http://$website
+done
+
